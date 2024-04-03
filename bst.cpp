7dca96facf440d8a1db943171208fc4f99402393
LiamQuinlan <122911760+LiamQuinlan@users.noreply.github.com>
2024-04-03 18:16:59 +0000

    documentation

--- bst.cpp
+++ bst.cpp
@@ -30,6 +30,8 @@ BST<T,U>::BST( void )
 //=========================================================================
 template <class T, class U>
 BST<T,U>::~BST( void )
+// Pre condition: BST exists
+// Post condition: all data from BST is deallocated
 {
     deleteTree(root);
 }
@@ -44,8 +46,8 @@ BST<T,U>::~BST( void )
 //=========================================================================
 template <class T, class U>
 bool BST<T,U>::empty( void )
-// Pre condition: 
-// Post condition: 
+// Pre condition: BST exists
+// Post condition: Returns if BST is empty
 {
     return root == nullptr;
 }
@@ -53,14 +55,14 @@ bool BST<T,U>::empty( void )
 
 //=========================================================================
 // insert
-// Parameters:
-// Returns:
-// 
+// Parameters: data value of type T and key value of type U
+// Returns: None, but inserts into existing tree
+// Inserts element with data d and key k into tree
 //=========================================================================
 template <class T, class U>
 void BST<T,U>::insert( const T& d, const U& k )
-// Pre condition: 
-// Post condition: 
+// Pre condition: BST is a valid binary search tree
+// Post condition: BST contains an element with data d and key k
 {
     Node* z = new Node(d, k); // Create a new node
     if (root == nullptr) 
@@ -101,49 +103,61 @@ void BST<T,U>::insert( const T& d, const U& k )
 
 //=========================================================================
 // get
-// Parameters:
-// Returns:
-// 
+// Parameters: key value of type U
+// Returns: data value of type T
+// given a key, returns the data of an element in the tree
 //=========================================================================
 template <class T, class U>
 T BST<T,U>::get( const U& k ) const
-// Pre condition: 
-// Post condition: 
+// Pre condition: BST is a valid binary search tree, and k is of type U
+// Post condition: No changes, but returns the data of the element with the key k
 {   
     return getHelper(root, k);
 }
 
+
 //=========================================================================
-// get
-// Parameters:
-// Returns:
-// 
+// getHelper
+// Parameters: Node x, key k
+// Returns: data value of type T
+// Helper function to get data given a value
 //=========================================================================
+// Pre condition: BST is a valid binary search tree, and k is of type U
+// Post condition: No changes, but returns the data of the element with the key k
 template <class T, class U>
 T BST<T,U>::getHelper(Node* x, const U& k) const 
 {
-    if (x == nullptr)
+    if (x == nullptr) // if tree is empty
     {
         return T();
     }
-    if (x->key == k) 
+    if (x->key == k) // if x matches k
     {
         return x->data;
     }
-    else if (x->key < k) 
+    else if (x->key < k) // go right
     {
         return getHelper(x->right, k);
     } 
-    else 
+    else // go left
     {
         return getHelper(x->left, k);
     }
 }
 
+
+//=========================================================================
+// transplant
+// Parameters: Node u, Node v
+// Returns: None, but alters tree
+// Helper function to transplant two nodes in a tree
+//=========================================================================
 template <class T, class U>
 void BST<T,U>::transplant(Node* u, Node* v)
+// Pre condition: u and v are valid elements of the BST
+// Post condition: u and v are now transplanted
 {
-    if (u->p == nullptr) {
+    if (u->p == nullptr) { // if tree is empty
         root = v;
     } else if (u == u->p->left) {
         u->p->left = v;
@@ -155,17 +169,17 @@ void BST<T,U>::transplant(Node* u, Node* v)
     }
 }
 
+
 //=========================================================================
 // remove
-// Parameters:
-// Returns:
-// 
+// Parameters: key value of type U
+// Returns: none, but alters tree
+// removes Node with key k from tree, maintaining BST properties
 //=========================================================================
-
 template <class T, class U>
 void BST<T,U>::remove( const U& k )
-// Pre condition: 
-// Post condition: 
+// Pre condition: BST is a valid binary search tree, k is of type U
+// Post condition: the node containing k is no longer in the BST, BST properties are maintained
 {
     Node* z = root;
     Node* parent = nullptr;
@@ -184,7 +198,7 @@ void BST<T,U>::remove( const U& k )
         }
     }
 
-    if (z == nullptr)
+    if (z == nullptr) // if tree is empty
     {
         return;
     }
@@ -216,53 +230,50 @@ void BST<T,U>::remove( const U& k )
     }
 
     delete z; // Free the memory
-
 }
 
 
 //=========================================================================
 // max_data
-// Parameters:
-// Returns:
-// 
+// Parameters: none
+// Returns: data value of type T
+// returns max data value in tree
 //=========================================================================
 template <class T, class U>
 T BST<T,U>::max_data( void ) const
-// Pre condition: 
-// Post condition: 
+// Pre condition: BST is a valid binary search tree
+// Post condition: no changes to tree, but returns max data value in tree 
 {
-    if (root == nullptr) 
+    if (root == nullptr) // if tree is empty
     {
         return T();
     }
     Node* x = root;
-    while (x->right != nullptr) 
+    while (x->right != nullptr) // follow right children until at leaf
     {
        x = x->right;
     }
     return x->data;        
-
 }
 
 
 //=========================================================================
 // min_data
-// Parameters:
-// Returns:
-// 
+// Parameters: none
+// Returns: data value of type T
+// returns min data value in tree
 //=========================================================================
 template <class T, class U>
 T BST<T,U>::min_data( void ) const
-// Pre condition: 
-// Post condition: 
+// Pre condition: BST is a valid binary search tree
+// Post condition: no changes to tree, but returns min data value in tree 
 {
-    if (root == nullptr) 
+    if (root == nullptr) // if tree is empty
     {
-
         return T();
     }
     Node* x = root;
-    while (x->left != nullptr) 
+    while (x->left != nullptr) // follow left children until at leaf
     {
         x = x->left;
     }
@@ -272,21 +283,21 @@ T BST<T,U>::min_data( void ) const
 
 //=========================================================================
 // max_key
-// Parameters:
-// Returns:
-// 
+// Parameters: none
+// Returns: key value of type U
+// returns max key value in tree
 //=========================================================================
 template <class T, class U>
 U BST<T,U>::max_key( void ) const
-// Pre condition: 
-// Post condition: 
+// Pre condition: BST is a valid binary search tree
+// Post condition: no changes to tree, but returns max key value in tree 
 {
-    if (root == nullptr) 
+    if (root == nullptr) // if tree is empty
     {
         return U();
     }
     Node* x = root;
-    while (x->right != nullptr) 
+    while (x->right != nullptr) // follow right children until at leaf
     {
         x = x->right;
     }
@@ -296,37 +307,38 @@ U BST<T,U>::max_key( void ) const
 
 //=========================================================================
 // min_key
-// Parameters:
-// Returns:
-// 
+// Parameters: none
+// Returns: key value of type U
+// returns min key value in tree
 //=========================================================================
 template <class T, class U>
 U BST<T,U>::min_key( void ) const
-// Pre condition: 
-// Post condition: 
+// Pre condition: BST is a valid binary search tree
+// Post condition: no changes to tree, but returns min key value in tree 
 {
-    if (root == nullptr) 
+    if (root == nullptr) // if tree is empty
     {
         return U();
     }
     Node* x = root;
-    while (x->left != nullptr) 
+    while (x->left != nullptr) // folllow left children until at leaf
     {
         x = x->left;
     }
     return x->key;
 }
 
+
 //=========================================================================
 // successor
-// Parameters:
-// Returns:
-// 
+// Parameters: key value of type U
+// Returns: key value of type U
+// returns the key of the successor of the element with key k
 //=========================================================================
 template <class T, class U>
 U BST<T,U>::successor( const U& k ) const
-// Pre condition: 
-// Post condition: 
+// Pre condition: bst is a valid binary search tree, k is of type U
+// Post condition: no changes to tree, but returns the key of the successor of the element with key k
 {
     Node* x = root;
     Node* parent = nullptr;
@@ -345,12 +357,12 @@ U BST<T,U>::successor( const U& k ) const
         }
     }
 
-    if (x == nullptr) 
+    if (x == nullptr) // if tree is empty
     {
         return U();
     }
 
-    if (x->right != nullptr) 
+    if (x->right != nullptr) // if right child is null
     {
         return getHelperSpecificNode(x->right);
     }
@@ -370,8 +382,17 @@ U BST<T,U>::successor( const U& k ) const
     return y->key;
 }
 
+
+//=========================================================================
+// getHelperSpecificNode
+// Parameters: Node
+// Returns: key value of type U
+// helper function for successor
+//=========================================================================
 template <class T, class U>
 U BST<T,U>::getHelperSpecificNode(Node* node) const {
+// Pre condition: bst is a valid binary search tree, node is a valid node in the tree
+// Post condition: returns key of the successor of node
     if (node == nullptr) {
         return U(); // Or throw an exception if that makes more sense for your application
     }
@@ -386,14 +407,14 @@ U BST<T,U>::getHelperSpecificNode(Node* node) const {
 
 //=========================================================================
 // in_order
-// Parameters:
-// Returns:
-// 
+// Parameters: none
+// Returns: string of values of BST in order
+// performs an in order tree walk, and returns a string of the elements of the tree, sorted ascending
 //=========================================================================
 template <class T, class U>
 string BST<T,U>::in_order( void ) const
-// Pre condition: 
-// Post condition: 
+// Pre condition: tree is a valid binary search tree
+// Post condition: no changes to tree, but prints the elements of the bst in ascending order
 {
     stringstream order;
     U x = getHelperSpecificNode(root);
@@ -412,17 +433,16 @@ string BST<T,U>::in_order( void ) const
 
 //=========================================================================
 // trim
-// Parameters:
-// Returns:
-// 
+// Parameters: U low, U high (range of data values to trim tree)
+// Returns: none, but removes all elements from tree with data values not in range
+// removes all elements from tree with data values not in range
 //=========================================================================
 template <class T, class U>
 void BST<T,U>::trim( const U low, const U high )
-// Pre condition: 
-// Post condition: 
+// Pre condition: bst is a valid binary search tree, low and high are both of type U, and low is less than high
+// Post condition: bst is trimmed so all elements are within the range (low, high)
 {
     root = trimHelper(root, low, high);
-    
 }
 
 
@@ -435,6 +455,8 @@ void BST<T,U>::trim( const U low, const U high )
 //=========================================================================
 template <class T, class U>
 void BST<T,U>::deleteTree( Node* x)
+// Pre condition: tree is a valid binary search tree, x is the root of this tree
+// Post condition: tree is deleted and fully deallocated
 {
     if (x != nullptr)
     {
@@ -444,17 +466,17 @@ void BST<T,U>::deleteTree( Node* x)
     }
 }
 
+
 // =========================================================================
 // to_string
-// Parameters:
-// Returns:
-
+// Parameters: none
+// Returns: string
+// returns a string containing values of BST from top to bottom, left to right
 // =========================================================================
-
 template <class T, class U>
 string BST<T,U>::to_string( void ) const
-// Pre condition: 
-// Post condition: 
+// Pre condition: tree is a valid binary search tree
+// Post condition: no changes to tree, but returns a string containing the values of the BST from top to bottom, left to right
 {
     stringstream result;
     queue<Node*> temp;
@@ -490,14 +512,17 @@ string BST<T,U>::to_string( void ) const
     return result.str();
 }
 
+
 //=========================================================================
-// trim
-// Parameters:
-// Returns:
-// 
+// trimHelper
+// Parameters: Node, U low, U high (range of data values to trim tree)
+// Returns: typename
+// helper function for trim
 //=========================================================================
 template <class T, class U>
 typename BST<T,U>::Node* BST<T,U>::trimHelper(Node* node, U low, U high) {
+// Pre condition: tree is a valid bst, node is the root of the BST, low is less than high
+// Post condition: tree is trimmed 
     // If the current node is null, return null.
     if (node == nullptr) {
         return nullptr;
@@ -526,3 +551,4 @@ typename BST<T,U>::Node* BST<T,U>::trimHelper(Node* node, U low, U high) {
 
     return node;
 }
+
